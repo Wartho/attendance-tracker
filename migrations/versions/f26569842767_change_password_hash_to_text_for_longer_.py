f713ff31c04653f7b8125789d0902e9896f45299
Kevin Malone <kmalone999@gmail.com>
2025-07-19 01:48:40 -0700

    Fix migration: explicitly alter password_hash to Text for PostgreSQL

---
+++ migrations/versions/f26569842767_change_password_hash_to_text_for_longer_.py
@@ -0,0 +1,24 @@
+"""Change password_hash to Text for longer hashes
+
+Revision ID: f26569842767
+Revises: 5104a34db420
+Create Date: 2025-07-19 01:42:40.236601
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f26569842767'
+down_revision = '5104a34db420'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('user', 'password_hash', type_=sa.Text())
+
+
+def downgrade():
+    op.alter_column('user', 'password_hash', type_=sa.String(length=128))
